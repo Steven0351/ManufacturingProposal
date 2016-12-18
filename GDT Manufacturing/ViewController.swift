54a3f8f0a9ff30c35dc4d24c8da518bece9ff0d8
Steven Sherry <affinityforapps@gmail.com>
2016-12-17 19:01:27 -0600

    Initial Commit

---
+++ GDT Manufacturing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GDT Manufacturing
+//
+//  Created by Steven Sherry on 12/17/16.
+//  Copyright © 2016 Affinity for Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
