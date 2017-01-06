2c2144b2d92a9f734891f8a406a4aa8ad95f1975
Steven Sherry <affinityforapps@gmail.com>
2017-01-05 22:42:08 -0600

    added multiple pickerViews for the fiberViewController…I don’t think this is the correct route to go. It’s a lot of code for something I should probably be able to do with one pickerView.

--- GDT Manufacturing/FiberViewController.swift
+++ GDT Manufacturing/FiberViewController.swift
@@ -18,30 +18,57 @@ class FiberViewController: UIViewController, UIPickerViewDelegate, UIPickerViewD
     @IBOutlet weak var lengthTextField: UITextField!
     @IBOutlet weak var uomTextField: UITextField!
     @IBOutlet weak var quantityTextField: UITextField!
-    @IBOutlet weak var fiberPickerView: UIPickerView!
     @IBOutlet weak var logoImageView: UIImageView!
+    
+    @IBOutlet weak var fiberTypePicker: UIPickerView!
+    @IBOutlet weak var fiberCountPicker: UIPickerView!
+    @IBOutlet weak var jacketRatingPicker: UIPickerView!
+    @IBOutlet weak var connectorAPicker: UIPickerView!
+    @IBOutlet weak var connectorBPicker: UIPickerView!
+    @IBOutlet weak var uomPicker: UIPickerView!
+    
     
     var uomArray = [""]
     var connectorArray = [""]
     var jacketRatingArray = [""]
     var fiberCountArray = [""]
     var fiberTypeArray = [""]
+    var errorArray = [""]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //Define arrays for UIPickerView
-        
+
         logoImageView.image = UIImage(named: "gdtmfg-logo")
+        
+        //Arrays for pickerViews
         
         uomArray = ["Feet","Meters"]
         connectorArray = ["LC/UPC", "SC/UPC","LC/APC","SC/APC","MTP Male","MTP Female","ST","FC","MT-RJ"]
         jacketRatingArray = ["Riser","Plenum","LSZH","I/O"]
         fiberCountArray = ["1F","2F","8F","12F","24F","48F","96F","144F"]
         fiberTypeArray = ["Simplex","Duplex - Round","Duplex - Zip","Breakout","Loose Tube","Microarray","Ribbon"]
+        errorArray = ["Error"]
         
-        fiberPickerView.delegate = self
-        fiberPickerView.dataSource = self
-        fiberPickerView.isHidden = true
+        //Set delegates to self and hide pickers
+        
+        fiberTypePicker.delegate = self
+        fiberTypePicker.dataSource = self
+        fiberTypePicker.isHidden = true
+        fiberCountPicker.delegate = self
+        fiberCountPicker.dataSource = self
+        fiberCountPicker.isHidden = true
+        jacketRatingPicker.delegate = self
+        jacketRatingPicker.dataSource = self
+        jacketRatingPicker.isHidden = true
+        connectorAPicker.delegate = self
+        connectorAPicker.dataSource = self
+        connectorAPicker.isHidden = true
+        connectorBPicker.delegate = self
+        connectorBPicker.dataSource = self
+        connectorBPicker.isHidden = true
+        uomPicker.delegate = self
+        uomPicker.dataSource = self
+        uomPicker.isHidden = true
         fiberTypeTextField.delegate = self
         fiberCountTextField.delegate = self
         jacketRatingTextField.delegate = self
@@ -50,40 +77,90 @@ class FiberViewController: UIViewController, UIPickerViewDelegate, UIPickerViewD
         lengthTextField.delegate = self
         uomTextField.delegate = self
         quantityTextField.delegate = self
-        fiberPickerView.removeFromSuperview()
         
+        /* Commenting out
+        fiberTypeTextField.inputView = fiberTypePicker
+        fiberCountTextField.inputView = fiberCountPicker
+        jacketRatingTextField.inputView = jacketRatingPicker
+        connectorSideaTextField.inputView = connectorAPicker
+        connectorSidebTextField.inputView = connectorBPicker
+        uomTextField.inputView = uomPicker
+        */
         
-        fiberTypeTextField.inputView = fiberPickerView
-        fiberCountTextField.inputView = fiberPickerView
-        jacketRatingTextField.inputView = fiberPickerView
-        connectorSideaTextField.inputView = fiberPickerView
-        connectorSidebTextField.inputView = fiberPickerView
-        lengthTextField.inputView = fiberPickerView
-        uomTextField.inputView = fiberPickerView
-        quantityTextField.inputView = fiberPickerView
+        fiberTypePicker.removeFromSuperview()
+        fiberCountPicker.removeFromSuperview()
+        jacketRatingPicker.removeFromSuperview()
+        connectorAPicker.removeFromSuperview()
+        connectorBPicker.removeFromSuperview()
+        uomPicker.removeFromSuperview()
+        
     }
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
-        return fiberCountArray.count
+        if fiberTypeTextField.isSelected == true {
+            return fiberCountArray.count
+        } else if fiberCountTextField.isSelected == true {
+            return fiberTypeArray.count
+        } else if jacketRatingTextField.isSelected == true {
+            return jacketRatingArray.count
+        } else if connectorSideaTextField.isSelected == true || connectorSidebTextField.isSelected == true {
+            return connectorArray.count
+        } else if uomTextField.isSelected == true {
+            return uomArray.count
+        }
+        return errorArray.count
     }
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return fiberCountArray[row]
+        if fiberTypeTextField.isSelected == true {
+            return fiberCountArray[row]
+        } else if fiberCountTextField.isSelected == true {
+            return fiberTypeArray[row]
+        } else if jacketRatingTextField.isSelected == true {
+            return jacketRatingArray[row]
+        } else if connectorSideaTextField.isSelected == true || connectorSidebTextField.isSelected == true {
+            return connectorArray[row]
+        } else if uomTextField.isSelected == true {
+            return uomArray[row]
+        }
+        return errorArray[row]
     }
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        fiberCountTextField.text = fiberCountArray[row]
-        
-    }
-    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
-        if fiberCountTextField.isSelected == true {
-            fiberPickerView.isHidden = false
+        if fiberTypeTextField.isSelected == true {
+            fiberTypeTextField.text = fiberCountArray[row]
+        } else if fiberCountTextField.isSelected == true {
+            fiberCountTextField.text = fiberTypeArray[row]
+        } else if jacketRatingTextField.isSelected == true {
+            jacketRatingTextField.text = jacketRatingArray[row]
+        } else if connectorSideaTextField.isSelected == true  {
+            connectorSideaTextField.text = connectorArray[row]
+        } else if connectorSidebTextField.isSelected == true {
+            connectorSidebTextField.text = connectorArray[row]
+        } else if uomTextField.isSelected == true {
+            uomTextField.text = uomArray[row]
         }
-        
-        return true
+        fiberTypeTextField.text = errorArray[row]
     }
     
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        if fiberCountTextField.isSelected == true {
+            fiberCountPicker.isHidden = false
+        } else if fiberCountTextField.isSelected == true {
+            fiberTypePicker.isHidden = false
+        } else if jacketRatingTextField.isSelected == true {
+            jacketRatingPicker.isHidden = false
+        } else if connectorSideaTextField.isSelected == true  {
+            connectorAPicker.isHidden = false
+        } else if connectorSidebTextField.isSelected == true {
+            connectorBPicker.isHidden = false
+        } else if uomTextField.isSelected == true {
+            uomPicker.isHidden = false
+        }
+        return false
+    }
+   
     @IBAction func addTapped(_ sender: Any) {
     }
 
