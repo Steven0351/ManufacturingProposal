da9319aedc52db91a5778b60103918557208b32b
Steven Sherry <affinityforapps@gmail.com>
2017-01-08 21:55:31 -0600

    Finally got the UIPickerView to load a different array based on the textField that is selected!!

--- GDT Manufacturing/FiberViewController.swift
+++ GDT Manufacturing/FiberViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FiberViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
+class FiberViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource,UITextFieldDelegate {
     
     @IBOutlet weak var fiberTypeTextField: UITextField!
     @IBOutlet weak var fiberCountTextField: UITextField!
@@ -20,10 +20,7 @@ class FiberViewController: UIViewController, UIPickerViewDelegate, UIPickerViewD
     @IBOutlet weak var quantityTextField: UITextField!
     @IBOutlet weak var logoImageView: UIImageView!
     
-
     @IBOutlet weak var fiberTypePicker: UIPickerView!
-    
-    var activeTextField = UITextField()
     
     var uomArray = [""]
     var connectorArray = [""]
@@ -51,8 +48,7 @@ class FiberViewController: UIViewController, UIPickerViewDelegate, UIPickerViewD
         
         fiberTypePicker.delegate = self
         fiberTypePicker.dataSource = self
-        fiberTypePicker.isHidden = true
-
+        
         fiberTypeTextField.delegate = self
         fiberCountTextField.delegate = self
         jacketRatingTextField.delegate = self
@@ -62,8 +58,9 @@ class FiberViewController: UIViewController, UIPickerViewDelegate, UIPickerViewD
         uomTextField.delegate = self
         quantityTextField.delegate = self
         
+        //set inputView for selected textFields to fiberTypePicker
         
-        //fiberTypeTextField.inputView = fiberTypePicker
+        fiberTypeTextField.inputView = fiberTypePicker
         fiberCountTextField.inputView = fiberTypePicker
         jacketRatingTextField.inputView = fiberTypePicker
         connectorSideaTextField.inputView = fiberTypePicker
@@ -75,95 +72,61 @@ class FiberViewController: UIViewController, UIPickerViewDelegate, UIPickerViewD
         
     }
     
-    //Using textFieldShouldBeginEditing isn't allowing the user to click on the textField...
-    
-    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool{
-        if fiberTypeTextField.isEditing == true {
-            activeTextField = fiberTypeTextField
-            currentArray = fiberTypeArray
-            fiberTypePicker.reloadAllComponents()
-            fiberTypePicker.isHidden = false
-        } else if fiberCountTextField.isEditing == true {
-            activeTextField = fiberCountTextField
-            currentArray = fiberCountArray
-            fiberTypePicker.reloadAllComponents()
-            fiberTypePicker.isHidden = false
-        } else if jacketRatingTextField.isEditing {
-            activeTextField = jacketRatingTextField
-            currentArray = jacketRatingArray
-            fiberTypePicker.reloadAllComponents()
-            fiberTypePicker.isHidden = false
-        } else if connectorSideaTextField.isEditing == true {
-            activeTextField = connectorSideaTextField
-            currentArray = connectorArray
-            fiberTypePicker.reloadAllComponents()
-            fiberTypePicker.isHidden = false
-        } else if connectorSidebTextField.isEditing == true {
-            activeTextField = connectorSidebTextField
-            currentArray = connectorArray
-            fiberTypePicker.reloadAllComponents()
-            fiberTypePicker.isHidden = false
-        } else if uomTextField.isEditing {
-            activeTextField = uomTextField
-            currentArray = uomArray
-            fiberTypePicker.reloadAllComponents()
-            fiberTypePicker.isHidden = false
-        }
-        return false
-    }
-
+   
+    //return number of columns in pickerview
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
+    
+    //return number of rows in pickerview
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
-                return currentArray.count
-    }
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-                return currentArray[row]
-    }
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-              activeTextField.text = currentArray[row]
-                
-    }
- 
-    /* Still trying to figure out the best way to utilize one pickerView for all textFields...
-     
-    @IBAction func fiberTypeEditing(_ sender: Any) {
-        func numberOfComponents(in pickerView: UIPickerView) -> Int {
-            return 1
-        }
-        func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
+        if fiberTypeTextField.isEditing == true {
             return fiberTypeArray.count
+        } else if fiberCountTextField.isEditing == true{
+            return fiberCountArray.count
+        } else if jacketRatingTextField.isEditing == true {
+            return jacketRatingArray.count
+        } else if connectorSideaTextField.isEditing || connectorSidebTextField.isEditing == true {
+            return connectorArray.count
+        } else if uomTextField.isEditing == true {
+            return uomArray.count
         }
-        func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return errorArray.count
+    }
+    
+    //return row text in pickerview
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if fiberTypeTextField.isEditing == true {
             return fiberTypeArray[row]
+        } else if fiberCountTextField.isEditing == true{
+            return fiberCountArray[row]
+        } else if jacketRatingTextField.isEditing == true {
+            return jacketRatingArray[row]
+        } else if connectorSideaTextField.isEditing || connectorSidebTextField.isEditing == true {
+            return connectorArray[row]
+        } else if uomTextField.isEditing == true {
+            return uomArray[row]
         }
-        
-        func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        return errorArray[row]
+    }
+    
+    //return text in textfield from pickerview
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if fiberTypeTextField.isEditing == true {
             fiberTypeTextField.text = fiberTypeArray[row]
+        } else if fiberCountTextField.isEditing == true{
+            fiberCountTextField.text = fiberCountArray[row]
+        } else if jacketRatingTextField.isEditing == true {
+            jacketRatingTextField.text = jacketRatingArray[row]
+        } else if connectorSideaTextField.isEditing == true {
+            connectorSideaTextField.text = connectorArray[row]
+        } else if connectorSidebTextField.isEditing == true {
+            connectorSidebTextField.text = connectorArray[row]
+        } else if uomTextField.isEditing == true {
+            uomTextField.text = uomArray[row]
         }
-        
-    }
-
-    @IBAction func fiberCountEditing(_ sender: Any) {
-    }
-   
-    @IBAction func jacketRatingEditing(_ sender: Any) {
+        view.self.endEditing(true)
     }
     
-    @IBAction func connectorSideaEditing(_ sender: Any) {
-    }
-    
-    @IBAction func connectorSidebEditing(_ sender: Any) {
-    }
-    
-    @IBAction func uomEditing(_ sender: Any) {
-    }
-    */
-    
-    
-    @IBAction func addTapped(_ sender: Any) {
-    }
-
-
+ 
 }
